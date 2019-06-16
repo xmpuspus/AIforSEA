fe5bc115468d7a65aef2f3063a6cc342569835d7
Xavier Puspus <xmpuspus@Xaviers-MacBook-Pro.local>
2019-06-16 10:55:46 +0800

    Added notebook, utils and model

---
+++ utils/utils.py
@@ -0,0 +1,156 @@
+import glob
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+def load_from_directory(path):
+    """
+    
+    Loads all data into dataframes and merges them into one.
+    
+    """
+    all_files = glob.glob(path + "/*.csv")
+    list_dir = []
+    
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        list_dir.append(df)
+        
+    concat_df = pd.concat(list_dir, axis=0, ignore_index=True)
+    return concat_df
+
+
+def show_confusion_matrix(C,class_labels=['0','1']):
+    """
+    C: ndarray, shape (2,2) as given by scikit-learn confusion_matrix function
+    class_labels: list of strings, default simply labels 0 and 1.
+
+    Draws confusion matrix with associated metrics.
+    """
+#     import matplotlib.pyplot as plt
+#     import numpy as np
+    
+    assert C.shape == (2,2), "Confusion matrix should be from binary classification only."
+    
+    # true negative, false positive, etc...
+    tn = C[0,0]; fp = C[0,1]; fn = C[1,0]; tp = C[1,1];
+
+    NP = fn+tp # Num positive examples
+    NN = tn+fp # Num negative examples
+    N  = NP+NN
+
+    fig = plt.figure(figsize=(8,8))
+    ax  = fig.add_subplot(111)
+    ax.imshow(C, interpolation='nearest', cmap=plt.cm.gray)
+
+    # Draw the grid boxes
+    ax.set_xlim(-0.5,2.5)
+    ax.set_ylim(2.5,-0.5)
+    ax.plot([-0.5,2.5],[0.5,0.5], '-k', lw=2)
+    ax.plot([-0.5,2.5],[1.5,1.5], '-k', lw=2)
+    ax.plot([0.5,0.5],[-0.5,2.5], '-k', lw=2)
+    ax.plot([1.5,1.5],[-0.5,2.5], '-k', lw=2)
+
+    # Set xlabels
+    ax.set_xlabel('Predicted Label', fontsize=16)
+    ax.set_xticks([0,1,2])
+    ax.set_xticklabels(class_labels + [''])
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.tick_top()
+    # These coordinate might require some tinkering. Ditto for y, below.
+    ax.xaxis.set_label_coords(0.34,1.06)
+
+    # Set ylabels
+    ax.set_ylabel('True Label', fontsize=16, rotation=90)
+    ax.set_yticklabels(class_labels + [''],rotation=90)
+    ax.set_yticks([0,1,2])
+    ax.yaxis.set_label_coords(-0.09,0.65)
+
+
+    # Fill in initial metrics: tp, tn, etc...
+    ax.text(0,0,
+            'True Neg: %d\n(Num Neg: %d)'%(tn,NN),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(0,1,
+            'False Neg: %d'%fn,
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(1,0,
+            'False Pos: %d'%fp,
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+
+    ax.text(1,1,
+            'True Pos: %d\n(Num Pos: %d)'%(tp,NP),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    # Fill in secondary metrics: accuracy, true pos rate, etc...
+    ax.text(2,0,
+            'False Pos Rate: %.2f'%(fp / (fp+tn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(2,1,
+            'True Pos Rate: %.2f'%(tp / (tp+fn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(2,2,
+            'Accuracy: %.2f'%((tp+tn+0.)/N),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(0,2,
+            'Neg Pre Val: %.2f'%(1-fn/(fn+tn+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+    ax.text(1,2,
+            'Pos Pred Val: %.2f'%(tp/(tp+fp+0.)),
+            va='center',
+            ha='center',
+            bbox=dict(fc='w',boxstyle='round,pad=1'))
+
+
+    plt.tight_layout()
+    plt.show()
+    
+    
+def process_data(data, n_samples=120):
+    """
+    Convert input data into single array of the first n seconds of each of the features/signals.
+    """
+
+    n = data.bookingID.drop_duplicates().shape[0]
+    data_sampled_per_booking = (data.groupby('bookingID')
+                                .apply(lambda x: x.sample(n_samples))
+                                .drop('bookingID', axis=1)
+                                .reset_index()
+                                .sort_values(['bookingID', 'second']))
+    
+    feature_cols = ['Accuracy', 'Bearing',
+       'acceleration_x', 'acceleration_y', 'acceleration_z', 'gyro_x',
+       'gyro_y', 'gyro_z','Speed', 'acceleration', 'gyro']
+
+    n_feats = len(feature_cols)
+    
+    arrs = (data_sampled_per_booking[['bookingID'] + feature_cols]
+            .groupby('bookingID')
+            .apply(lambda x: x.values.reshape(n_samples, n_feats))
+            .values)
+    
+    reshaped_data = np.hstack(arrs).reshape(n, n_samples * n_feats)
+    return reshaped_data
